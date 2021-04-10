d79114f6a3c4db5e9a687cfd6467037bfa30fbff
Margus Heinmaa <margus@users.noreply.github.com>
2021-04-10 12:10:09 +0300

    add helper script and make cerbot script more dynamic

--- certbot-renew-hook-haproxy.sh
+++ certbot-renew-hook-haproxy.sh
@@ -1,37 +1,87 @@
 #!/usr/bin/env bash
-#
-# After renew hook for certbot for haproxy
-#
 
-CERTBOT_LIVE_FOLER='/etc/letsencrypt/live'
-HAPROXY_CERTS='/etc/haproxy/certs'
+#
+# Usage: {SELF} [-h|--help]
+#
+# After renew hook for certbot haproxy certificate generation
+# If your HAProxy Certificates folder location is located somewhere else then export the path with the
+# following variable
+#   - HAPROXY_CERTS_DIR
+#
+# Options:
+#        --verbose                      Verbose output
+#   -h | --help                         Displays this help text
 
-if [[ ! -d "${CERTBOT_LIVE_FOLER}" ]]; then
-    echo 'Unable to find any certbot certs'
-    exit 1
+SCRIPT_NAME="${0}"
+SCRIPT_FOLDER="$(dirname "${SCRIPT_NAME}")"
+
+# shellcheck disable=SC1090
+. "${SCRIPT_FOLDER}/helper.sh"
+
+export MESSAGE_LOG_FILE="${LOG_DIR}/${SCRIPT_NAME}.log"
+
+CERTBOT_LIVE_DIR='/etc/letsencrypt/live'
+
+if [[ -z "${HAPROXY_CERTS_DIR}" ]]; then
+  HAPROXY_CERTS_DIR='/etc/haproxy/certs'
 fi
 
-if [[ ! -d "${HAPROXY_CERTS}" ]]; then
-    mkdir -p "${HAPROXY_CERTS}"
-fi
+VERBOSE=
 
-for CERT_PATH in "${CERTBOT_LIVE_FOLER}"/*; do
-    if [[ ! -d "${CERT_PATH}" ]]; then
-        continue
-    fi
+POSITIONAL=()
+while [[ $# -gt 0 ]]; do
+  KEY="${1}"
 
-    CERT="$(basename "${CERT_PATH}")"
-    echo "Cert: ${CERT}"
-
-    if [[ ! -f "${CERT_PATH}/fullchain.pem" ]] || [[ ! -f "${CERT_PATH}/privkey.pem" ]]; then
-        echo 'Can not find correct fullchain and/or privkey'
-        continue
-    fi
-
-    cat "${CERT_PATH}/fullchain.pem" "${CERT_PATH}/privkey.pem" > "${HAPROXY_CERTS}/${CERT}.pem"
+  case "${KEY}" in
+  -v | -vv | -vvv | --verbose)
+    VERBOSE=1
+    shift
+    ;;
+  -h | --help)
+    usage "${0}"
+    exit
+    ;;
+  -*)
+    message_error "Incorrect key: ${KEY}"
+    ;;
+  *)
+    POSITIONAL+=("${1}")
+    shift
+    ;;
+  esac
 done
 
-HAPROXY_SERVICE_NAME="$(systemctl --plain |grep haproxy |awk '{print $1}')"
+set -- "${POSITIONAL[@]}"
+
+if [ -n "${VERBOSE}" ]; then
+  set -xv
+fi
+
+if [[ ! -d "${CERTBOT_LIVE_DIR}" ]]; then
+  message_error 'Unable to find any certbot certs'
+fi
+
+if [[ ! -d "${HAPROXY_CERTS_DIR}" ]]; then
+  mkdir -p "${HAPROXY_CERTS_DIR}"
+fi
+
+for CERT_PATH in "${CERTBOT_LIVE_DIR}"/*; do
+  if [[ ! -d "${CERT_PATH}" ]]; then
+    continue
+  fi
+
+  CERT="$(basename "${CERT_PATH}")"
+  message_action "Generate Cert: ${CERT}"
+
+  if [[ ! -f "${CERT_PATH}/fullchain.pem" ]] || [[ ! -f "${CERT_PATH}/privkey.pem" ]]; then
+    message_error_notice 'Can not find correct fullchain and/or privkey'
+    continue
+  fi
+
+  cat "${CERT_PATH}/fullchain.pem" "${CERT_PATH}/privkey.pem" >"${HAPROXY_CERTS_DIR}/${CERT}.pem"
+done
+
+HAPROXY_SERVICE_NAME="$(systemctl --plain | grep haproxy | awk '{print $1}')"
 if [[ -n "${HAPROXY_SERVICE_NAME}" ]]; then
-    systemctl reload "${HAPROXY_SERVICE_NAME}"
+  systemctl reload "${HAPROXY_SERVICE_NAME}"
 fi
---
+++ helper.sh
@@ -0,0 +1,165 @@
+#!/usr/bin/env bash
+#
+# Generic bash script helpers
+#
+
+COLOR_RED="$(tput -Txterm setaf 1)"
+COLOR_GREEN="$(tput -Txterm setaf 2)"
+COLOR_YELLOW="$(tput -Txterm setaf 3)"
+COLOR_BLUE="$(tput -Txterm setaf 6)"
+FONT_BOLD="$(tput -Txterm bold)"
+FONT_RESET="$(tput -Txterm sgr0)"
+
+MESSAGE_LOG_DIR='/var/log'
+MESSAGE_TIMESTAMP=1
+
+usage() {
+  local SCRIPT="${1}"
+
+  HELP_TEXT="$(awk '/^# Usage:/,/^$/ { sub(/^# ?/,"");if(i++)print s;s=$0; }' "${SCRIPT}")"
+
+  echo
+  output_message_format "${HELP_TEXT//\{SELF\}/${SCRIPT}}" 'green'
+  echo
+}
+
+output_to_log() {
+  local LOG_CONTENT="${1}"
+
+  if [ ! -d "${MESSAGE_LOG_DIR}" ]; then
+    mkdir -p "${MESSAGE_LOG_DIR}"
+  fi
+
+  if [[ -n "${OS_TYPE}" ]] && [[ "${OS_TYPE}" == 'mac' ]]; then
+    if [[ -n "${MESSAGE_LOG_FILE}" ]]; then
+      echo "${LOG_CONTENT}" >>"${MESSAGE_LOG_FILE}"
+    fi
+  else
+    if [[ -n "${MESSAGE_LOG_FILE}" ]]; then
+      echo "${LOG_CONTENT}" | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g" >>"${MESSAGE_LOG_FILE}"
+    fi
+  fi
+}
+
+output_date_format() {
+  if [ -n "${MESSAGE_TIMESTAMP}" ]; then
+    echo "[$(date +"%m-%d-%y %T")] "
+  fi
+}
+
+confirm() {
+  local CONFIRM_MESSAGE="${1}"
+
+  while true; do
+    read -r -p "${CONFIRM_MESSAGE} (yes/no) " ANSWER
+    output_to_log "${CONFIRM_MESSAGE} (${ANSWER})"
+    case ${ANSWER} in
+    [Yy]*)
+      break
+      ;;
+    [Nn]*)
+      message_action ' - stopping'
+      exit
+      ;;
+    *)
+      message_notice 'Please answer yes or no.'
+      ;;
+    esac
+  done
+}
+
+output_message_format() {
+  local MESSAGE="${1}"
+  local COLOR="${2}"
+  local PREFIX="${3}"
+
+  local COLOR_FORMAT=
+  local PREFIX_TEXT=
+
+  case "${COLOR}" in
+  'yellow')
+    COLOR_FORMAT="${COLOR_YELLOW}"
+    ;;
+  'green')
+    COLOR_FORMAT="${COLOR_GREEN}"
+    ;;
+  'red')
+    COLOR_FORMAT="${FONT_BOLD}${COLOR_RED}"
+    ;;
+  'blue')
+    COLOR_FORMAT="${COLOR_BLUE}"
+    ;;
+  esac
+
+  if [[ -n "${PREFIX}" ]]; then
+    PREFIX_TEXT="[${PREFIX}] "
+  fi
+
+  echo "${COLOR_FORMAT}$(output_date_format)${PREFIX_TEXT}${MESSAGE}${FONT_RESET}"
+}
+
+message_action() {
+  local MESSAGE="${1}"
+
+  local OUTPUT_MESSAGE=
+  OUTPUT_MESSAGE="$(output_message_format "${MESSAGE}" 'yellow' 'action')"
+
+  output_to_log "${OUTPUT_MESSAGE}"
+  echo "${OUTPUT_MESSAGE}"
+}
+
+message_ok() {
+  local MESSAGE="${1}"
+
+  local OUTPUT_MESSAGE=
+  OUTPUT_MESSAGE="$(output_message_format "${MESSAGE}" 'green' 'ok')"
+
+  output_to_log "${OUTPUT_MESSAGE}"
+  echo "${OUTPUT_MESSAGE}"
+}
+
+message_notice() {
+  local MESSAGE="${1}"
+
+  local OUTPUT_MESSAGE=
+  OUTPUT_MESSAGE="$(output_message_format "${MESSAGE}" 'blue')"
+
+  output_to_log "${OUTPUT_MESSAGE}"
+  echo "${OUTPUT_MESSAGE}"
+}
+
+message() {
+  local MESSAGE="${1}"
+
+  local OUTPUT_MESSAGE=
+  OUTPUT_MESSAGE="$(output_message_format "${MESSAGE}")"
+
+  output_to_log "${OUTPUT_MESSAGE}"
+  echo "${OUTPUT_MESSAGE}"
+}
+
+message_error_notice() {
+  local MESSAGE="${1}"
+
+  local OUTPUT_MESSAGE=
+  OUTPUT_MESSAGE="$(output_message_format "${MESSAGE}" 'red' 'error')"
+
+  output_to_log "${OUTPUT_MESSAGE}"
+  echo "${OUTPUT_MESSAGE}" >&2
+}
+
+message_error() {
+  local MESSAGE="${1}"
+
+  message_error_notice "${MESSAGE}"
+  exit 1
+}
+
+check_argument() {
+  local KEY="${1}"
+  local VALUE="${2}"
+
+  if [[ -z "${VALUE}" ]]; then
+    message_error "${KEY} requires a value, current empty"
+  fi
+}
